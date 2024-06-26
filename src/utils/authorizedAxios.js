9d870c0cb025981066781e0da4c76c3c1e09bfef
hoangtran <huyhoangtran211201@gmail.com>
2024-06-27 02:03:37 +0700

    issue parallel api

--- src/utils/authorizedAxios.js
+++ src/utils/authorizedAxios.js
@@ -19,7 +19,6 @@ axiosClient.interceptors.request.use(
   },
 );
 
-let refreshTokenPromise = null;
 
 axiosClient.interceptors.response.use(
   (response) => {
@@ -32,26 +31,19 @@ axiosClient.interceptors.response.use(
   (error) => {
     const originRequest = error.config;
     if (error.response?.status === 410 && !originRequest._retry) {
-      if (!refreshTokenPromise) {
-        const refreshToken = localStorage.getItem("refreshToken");
-        refreshTokenPromise = refreshTokenAPI(refreshToken)
+      originRequest._retry = true;
+      const refreshToken = localStorage.getItem("refreshToken");
+        return refreshTokenAPI(refreshToken)
           .then((res) => {
-            return res.data?.accessToken
+            const accessToken = res.data?.accessToken
+            localStorage.setItem("accessToken", accessToken);
+            axiosClient.defaults.headers.Authorization = `Bearer ${accessToken}`;
+            return axiosClient(originRequest);
           })
           .catch((err) => {
             // remove data in local storage and redirect to login page
             location.href = "/login";
           })
-          .finally(() => {
-            refreshTokenPromise = null;
-          });
-      }
-
-      return refreshTokenPromise.then((accessToken) => {
-        localStorage.setItem("accessToken", accessToken);
-        axiosClient.defaults.headers.Authorization = `Bearer ${accessToken}`;
-        return axiosClient(originRequest);
-      });
     }
 
     return Promise.reject(error);
